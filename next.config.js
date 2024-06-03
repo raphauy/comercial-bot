304c192b1cd271b6f0ee6287725a1039778dfd92
raphauy <rapha.uy@rapha.uy>
2024-06-03 16:34:41 -0300

    aws deployment

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        outputFileTracingIncludes: {
-          "/api/chat": ["./node_modules/tiktoken/tiktoken_bg.wasm"],
-        },
-    },
 }
 
 module.exports = nextConfig
