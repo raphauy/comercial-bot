ddacbbaa24b09226bf9155a3f74e960a3db18c33
raphauy <rapha.uy@rapha.uy>
2025-01-02 15:13:17 -0300

    api sell update

--- src/app/api/[clientId]/sells/update/route.ts
+++ src/app/api/[clientId]/sells/update/route.ts
@@ -52,6 +52,7 @@ export async function POST(request: Request, { params }: { params: { clientId: s
         return NextResponse.json( { "data": updated }, { status: 200 })
 
     } catch (error) {
+        console.log("error: ", error)
         return NextResponse.json({ error: "error: " + error}, { status: 502 })        
     }
    
